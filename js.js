833130c9103e46f3422ffb3f6ac390c7d1eef657
Cristian Cioanca <cristian.cioanca@hotmail.com>
2016-06-13 16:22:32 -0400

    perliminaries and first page complete

--- js.js
+++ js.js
@@ -1,30 +1,37 @@
-$(window).on('scroll', function() {
-   scrollPosition = $(this).scrollTop();
-   if (scrollPosition >= 10 && scrollPosition <= 300) {
-      $(".navbar-custom").removeClass('nav-no-scoll');
-      $(".navbar-custom").addClass('nav-scrolled');
-   }
-   if (scrollPosition <= 10) {
-      $(".navbar-custom").removeClass('nav-scrolled');
-      $(".navbar-custom").addClass('nav-no-scoll');
-   }
-});
+//$(document).ready(function(){
+//    $(this).scrollTop(0);
+//});
 
-$(function() {
-   $('a[href*=#]:not([href=#])').click(function() {
-      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-         var target = $(this.hash);
-         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-         if (target.length) {
-            $('html,body').animate({
-               scrollTop: target.offset().top
-            }, 1000);
-            return false;
-         }
-      }
-   });
-});
+$(document).ready(function () {
 
-$(document).ready(function(){
-    $(this).scrollTop(0);
+    $(window).on('scroll', function () {
+        scrollPosition = $(this).scrollTop();
+        if (scrollPosition >= 10 && scrollPosition <= 300) {
+            $(".navbar-custom").removeClass('nav-no-scoll');
+            $(".navbar-custom").addClass('nav-scrolled');
+        }
+        if (scrollPosition <= 10) {
+            $(".navbar-custom").removeClass('nav-scrolled');
+            $(".navbar-custom").addClass('nav-no-scoll');
+        }
+    });
+
+    $(function () {
+        $('a[href*=#]:not([href=#])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
+    $('.nav-link').click(function () {
+        $(".navbar-collapse").collapse('hide');
+    });
 });
