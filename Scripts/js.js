309517c3471cac633b7e9e9d187143aebc877340
Cristian cioanca <cristian.cioanca@goodlifefitness.com>
2020-02-10 14:37:39 -0500

    added FA and lightbulb theme switching.

--- Scripts/js.js
+++ Scripts/js.js
@@ -10,10 +10,14 @@ $(document).ready(function () {
         if($('#theme').data('theme') == 'dark'){
             $('#theme').attr('href','Styles/css/minified/light-theme.min.css');
             $('#theme').data('theme', 'light');
+            $('.far.fa-lightbulb').show();
+            $('.fas.fa-lightbulb').hide();
         }
         else{
             $('#theme').attr('href','Styles/css/minified/dark-theme.min.css');
             $('#theme').data('theme', 'dark');
+            $('.far.fa-lightbulb').hide();
+            $('.fas.fa-lightbulb').show();
         }
     });
 });
