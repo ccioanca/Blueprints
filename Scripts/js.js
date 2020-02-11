0f6d1c2db1d6b8a9a5b559a4339b1c896b7c02dc
Cristian cioanca <cristian.cioanca@goodlifefitness.com>
2020-02-11 11:12:46 -0500

    prefered theme + switching + picked theme memory using local storage.

--- Scripts/js.js
+++ Scripts/js.js
@@ -2,22 +2,62 @@
 //    $(this).scrollTop(0);
 //});
 
+var theme = localStorage.getItem('theme');
+var dark_theme = 'Styles/css/minified/dark-theme.min.css';
+var light_theme = 'Styles/css/minified/light-theme.min.css';
+
+if((window.matchMedia("(prefers-color-scheme: dark)").matches || window.matchMedia("(prefers-color-scheme: no-preference)").matches) && theme == undefined){
+    theme = 'dark';
+    $('#theme').attr('href', dark_theme);
+}
+else if(window.matchMedia("(prefers-color-scheme: light)").matches && theme == undefined){
+    theme = 'light';
+    $('#theme').attr('href', light_theme);
+}
+
+if(theme == 'dark')
+    $('#theme').attr('href', dark_theme);
+else
+    $('#theme').attr('href', light_theme);
+
+
 $(document).ready(function () {
 
     
     // theme switcher prototype
+    // $('#theme-btn').click(function(){
+    //     if($('#theme').data('theme') == 'dark'){
+    //         $('#theme').attr('href','Styles/css/minified/light-theme.min.css');
+    //         $('#theme').data('theme', 'light');
+    //         $('.far.fa-lightbulb').show();
+    //         $('.fas.fa-lightbulb').hide();
+    //     }
+    //     else{
+    //         $('#theme').attr('href','Styles/css/minified/dark-theme.min.css');
+    //         $('#theme').data('theme', 'dark');
+    //         $('.far.fa-lightbulb').hide();
+    //         $('.fas.fa-lightbulb').show();
+    //     }
+    // });
+
     $('#theme-btn').click(function(){
-        if($('#theme').data('theme') == 'dark'){
-            $('#theme').attr('href','Styles/css/minified/light-theme.min.css');
-            $('#theme').data('theme', 'light');
-            $('.far.fa-lightbulb').show();
-            $('.fas.fa-lightbulb').hide();
+        //we can check for cookie if one is set.
+
+
+        if(theme == 'dark'){
+            theme = 'light';
+            //light
+            $('#theme').attr('href', light_theme);
+            localStorage.setItem("theme", 'light');
         }
         else{
-            $('#theme').attr('href','Styles/css/minified/dark-theme.min.css');
-            $('#theme').data('theme', 'dark');
-            $('.far.fa-lightbulb').hide();
-            $('.fas.fa-lightbulb').show();
+            theme = 'dark';
+            //dark
+            $('#theme').attr('href', dark_theme);
+            localStorage.setItem("theme", 'dark');
         }
+
+
+
     });
 });
