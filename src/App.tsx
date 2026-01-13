4269df52d1ef09d40a2f1456465ffcced5ea2b4a
Cris <cristian.cioanca@gmail.com>
2026-01-13 12:46:05 -0500

    Added Typescript

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Portfolio</h1>
+      <div className="card">
+        <p>
+          My Portfolio
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
