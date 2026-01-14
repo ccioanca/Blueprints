e63b054c87a57ad4c941354193c0fbc85496480e
Cris <cristian.cioanca@gmail.com>
2026-01-14 09:49:12 -0500

    Fixing the custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Blueprints"
+  base: "/" //because we're auto-redirecting with the CNAME record in gh-pages, we can just assume the base is at the root.
 })
