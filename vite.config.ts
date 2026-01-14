9eb3f47f32d919bf0add47ec180f377357962df3
Cris <cristian.cioanca@gmail.com>
2026-01-14 12:12:49 -0500

    Setting up Tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/" //because we're auto-redirecting with the CNAME record in gh-pages, we can just assume the base is at the root.
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  base: "/" //because we're auto-redirecting with the CNAME record in gh-pages, we can just assume the base is at the root. 
+  // If we didn't have a custom CNAME, we'd have to set `base: "/Blueprints"` (The name of the GH Pages Project)
 })
