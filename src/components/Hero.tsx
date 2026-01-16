756679fe25992e74a1aff288d3af1b4f10c8f94a
Cris <cristian.cioanca@gmail.com>
2026-01-16 14:31:07 -0500

    Small styling.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,15 @@
+import { Button } from '@/components/ui/button';
 import React from 'react';
 
 const Hero = () => {
   return (
-    <section id="hero" className="h-screen w-screen bg-gray-100 flex items-center justify-center">
+    <section id="hero" className="h-screen flex items-center justify-center">
       <div className="text-center">
-        <h2 className="text-5xl font-extrabold text-gray-900 mb-4">Hi, I'm Cristian</h2>
-        <p className="text-xl text-gray-700">An Engineering Manager.</p>
-        <button className="mt-8 px-6 py-3 bg-blue-600 text-white text-lg font-semibold rounded-lg hover:bg-blue-700 transition duration-300">View My Work</button>
+        <h2 className="text-5xl font-extrabold mb-4">Hi, I'm Cristian</h2>
+        <p className="text-xl pb-12">An Engineering Manager.</p>
+        <Button size={"xl"} className='dark:text-gray-100'>
+          View My Work
+        </Button>
       </div>
     </section>
   );
