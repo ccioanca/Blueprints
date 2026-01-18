05c21c88067d491044c0796c1d11560360c3154a
Cris <cristian.cioanca@gmail.com>
2026-01-17 22:06:07 -0500

    Cleaned some CSS up

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './styles/styles.scss'
 import App from './App'
 import { ThemeProvider } from './lib/theme-provider';
 
