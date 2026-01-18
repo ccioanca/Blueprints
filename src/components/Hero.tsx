aca2b62779d946ea8b555dcff59a53b753725128
Cris <cristian.cioanca@gmail.com>
2026-01-17 22:06:46 -0500

    Using Typewriter component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,26 @@
 import { Button } from '@/components/ui/button';
 import React from 'react';
+import Typewriter from './typewriter';
+
+const myStyle = {
+  fontFamily: 'monospace'
+};
 
 const Hero = () => {
   return (
     <section id="hero" className="h-screen flex items-center justify-center">
       <div className="text-center">
         <h2 className="text-5xl font-extrabold mb-4">Hi, I'm Cristian</h2>
-        <p className="text-xl pb-12">An Engineering Manager.</p>
+        <div className='pb-12' style={myStyle}>
+          <Typewriter className="text-xl" 
+          typeData={
+            ["An Engineering Manager",
+            "A Senior Developer", 
+            "A People Leader",
+            "A Creator"]} 
+          typeTime={2500}/>
+        </div>
+        
         <Button size={"xl"} className='dark:text-gray-100'>
           View My Work
         </Button>
