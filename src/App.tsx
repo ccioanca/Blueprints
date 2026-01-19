84d104a9128b6f8be0b181fd79cf9b23a3cb299e
Cris <cristian.cioanca@gmail.com>
2026-01-19 10:15:26 -0500

    Little style update - probably needs redoing

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 
 function App() {
-  
+
   return (
-    <div className="w-screen h-screen bg-white dark:bg-gray-800">
-      <Header />
-      <main className="flex-grow">
-        <Hero />
-        {/* Other sections will go here */}
+    <div className="w-full min-h-screen bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 relative">
+      <Header/>
+      <main>
+         <Hero />
+        {/*<About />
+        <Skills />
+        <Projects />
+        <Contact /> */}
       </main>
     </div>
   );
