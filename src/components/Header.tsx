4d420eb941beac3ab8bc4d79e4a3a25bb9a1a45d
Cris <cristian.cioanca@gmail.com>
2026-02-09 18:57:40 -0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,6 +18,7 @@ const Header = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
+
   const navItems = [
     { label: 'About', href: '#about' },
     { label: 'Projects', href: '#projects' },
@@ -27,14 +28,14 @@ const Header = () => {
   return (
     <>
       <NavigationMenu className="fixed top-0 left-0 w-full z-50 bg-white/20 dark:bg-gray-800/20 backdrop-blur-sm shadow-sm">
-        <NavigationMenuList className="flex w-full items-center justify-between p-3 max-w-7xl mx-auto">
+        <NavigationMenuList className="flex w-full items-center justify-between p-3 mx-auto">
           {/* Logo or Name */}
           {/* <NavigationMenuLink href="#hero" className="text-xl font-bold tracking-tight text-gray-900 dark:text-gray-100 hover:!bg-transparent">
             Cristian
           </NavigationMenuLink> */}
 
           {/* Desktop Navigation */}
-          <div className="hidden md:flex items-center space-x-6">
+          <div className="hidden md:flex items-center space-x-6 md:w-full flex-1 justify-center">
             {navItems.map((item, index) => (
                 <NavigationMenuLink key={index} href={item.href} className="text-md font-medium text-gray-700 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400 transition-colors">
                   {item.label}
@@ -43,7 +44,7 @@ const Header = () => {
             <NavigationMenuItem>
               <button
                 onClick={toggleTheme}
-                className="flex items-center justify-center rounded-full p-2 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors"
+                className="flex items-center justify-center rounded-full p-2 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors"
                 aria-label="Toggle theme"
               >
                 {theme === 'dark' ? <SunDimIcon className="h-5 w-5" /> : <MoonIcon className="h-5 w-5" />}
@@ -52,29 +53,30 @@ const Header = () => {
           </div>
 
           {/* Mobile Navigation Toggle */}
-          <div className="md:hidden flex items-center space-x-2">
-            <NavigationMenuItem>
-              <button
-                onClick={toggleTheme}
-                className="flex items-center justify-center rounded-full p-2 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors"
-                aria-label="Toggle theme"
-              >
-                {theme === 'dark' ? <SunDimIcon className="h-5 w-5" /> : <MoonIcon className="h-5 w-5" />}
-              </button>
-            </NavigationMenuItem>
+          <div className="md:hidden flex w-full items-center justify-between space-x-2">
             <NavigationMenuItem>
               <button
                 onClick={toggleMobileMenu}
-                className="flex items-center justify-center rounded-md p-2 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                className="flex items-center justify-center rounded-md p-2 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700"
                 aria-label="Open mobile menu"
               >
                 <MenuIcon className="h-6 w-6" />
               </button>
             </NavigationMenuItem>
+            <NavigationMenuItem>
+              <button
+                onClick={toggleTheme}
+                className="flex items-center justify-center rounded-full p-2 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors"
+                aria-label="Toggle theme"
+              >
+                {theme === 'dark' ? <SunDimIcon className="h-5 w-5" /> : <MoonIcon className="h-5 w-5" />}
+              </button>
+            </NavigationMenuItem>
+            
           </div>
         </NavigationMenuList>
       </NavigationMenu>
-      <MobileNav navItems={navItems} isOpen={isMobileMenuOpen} onClose={toggleMobileMenu} />
+      <MobileNav navItems={navItems} isOpen={isMobileMenuOpen} onClose={toggleMobileMenu}/>
     </>
   );
 };
