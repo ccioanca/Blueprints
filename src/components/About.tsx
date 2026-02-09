069886f4bc081c71e9b6851247e52d4c2eb32a12
Cris <cristian.cioanca@gmail.com>
2026-02-09 13:06:43 -0500

    Edited About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Timeline from './ui/timeline';
+import Competencies from './ui/competencies';
+
 
 const About = () => {
   return (
-    <section id="about" className="py-16 bg-gray-50 dark:bg-gray-900">
+    <section id="about" className="py-16 bg-indigo-100 dark:bg-gray-800">
       <div className='w-full md:w-1/2 px-8 sm:px-16 md:mx-auto md:px-0'>
-        <h2 className="mb-4">
+        <h2 className="mb-8">
           About Me
         </h2>
 
@@ -12,17 +15,46 @@ const About = () => {
           Driving Operational Excellence
         </h3>
         <p className="mb-4">
-          Hello! I'm Cristian, an Developer Lead with a passion for building innovative solutions and leading high-performing specialized teams in complex ecosystems. My current focus is on operational excellence & consistency while helping deliver scalable initiatives.
+          Hello! I'm Cristian, a Developer Lead with a passion for building innovative solutions and leading high-performing specialized teams in complex ecosystems. My current focus is on operational excellence & consistency while helping deliver scalable solutions & competent teams.
         </p>
         <p className="mb-4">
           I've had the privilege of working on a diverse range of projects, from intricate POCs to large-scale enterprise applications.
         </p>
         <p className="mb-4">
-          My expertise lies in crafting robust, scalable, and user-centric applications using modern web technologies. I thrive in environments where I can mentor developers, foster a culture of technical excellence, and drive strategic initiatives that align with business goals. I am constantly exploring new technologies and methodologies to stay at the forefront of the industry.
+          My expertise lies in crafting robust, scalable, and user-centric applications. I thrive in environments where I can mentor developers, foster a culture of technical excellence, and drive strategic initiatives that align with business goals. I am constantly exploring new technologies and methodologies to stay at the forefront of the industry.
         </p>
         <p>
           When I'm not coding or leading, you can find me in the gym, hitting the winter slopes, creating digital art, or dabbling in game development (which I guess counts as coding).
         </p>
+
+        {/* <hr className='my-6' />
+
+        <h3 className="mb-4">
+          Impact by the Numbers
+        </h3>
+        <ul className="list-inside">
+          <li className="list-disc mb-2">90%+ Sprint Goal Completion Rate achieved through new delivery standards. </li>
+          <li className="list-disc mb-2">20% Acceleration in Time-to-Market via improved release frameworks. </li>
+          <li className="list-disc mb-2">2 High-Impact Teams Managed across distributed locations.</li>
+        </ul> */}
+
+        <hr className='my-6'/>
+
+        <h3 className="mb-4">
+          Professional Competencies
+        </h3>
+
+        <Competencies/>
+
+        <hr className='my-6'/>
+
+        <h3 className="mb-4">
+          Professional Journey
+        </h3>
+
+        <Timeline/>
+
+
       </div>
     </section>
   );
