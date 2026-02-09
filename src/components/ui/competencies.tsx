2500786e1eba99bb7be96806b308da53209cb84f
Cris <cristian.cioanca@gmail.com>
2026-02-09 13:06:19 -0500

    Added competencies component

---
+++ src/components/ui/competencies.tsx
@@ -0,0 +1,31 @@
+
+const Competencies = () => {
+    return (
+        <table>
+          <tbody>
+            <tr>
+              <td>Strategic Leadership</td>
+              <td>test</td>
+            </tr>
+            <tr>
+              <td>Technical Management</td>
+              <td>test</td>
+            </tr>
+            <tr>
+              <td>Backend Development</td>
+              <td>test</td>
+            </tr>
+            <tr>
+              <td>Frontend Development</td>
+              <td>test</td>
+            </tr>
+            <tr>
+              <td>Design</td>
+              <td>test</td>
+            </tr>
+          </tbody>
+        </table>
+    )
+}
+
+export default Competencies;
