c37855d3328ad978fdd6dce07b4e8bb04c2fa6ab
Cris <cristian.cioanca@gmail.com>
2026-02-09 22:42:01 -0500

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Timeline from './ui/timeline';
-import Competencies from './ui/competencies';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/card';
+import Competencies from '@/components/competencies';
 
 
 const About = () => {
@@ -38,8 +37,6 @@ const About = () => {
 
         <hr className='my-12'/>
 
-        {/* <svg width="453" height="8" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-full max-sm:hidden lg:origin-left lg:scale-x-130"><path d="M2 6.75068C53.4722 -1.10509 368.533 2.14284 451.5 6.75085" stroke="currentColor" stroke-width="2.5" stroke-linecap="round"></path></svg> */}
-
         <h3 className="mb-4">
           Professional Competencies
         </h3>
@@ -53,10 +50,6 @@ const About = () => {
         </h3>
 
         <Timeline/>
-
-        <hr className='my-12' />
-
-
 
       </div>
     </section>
