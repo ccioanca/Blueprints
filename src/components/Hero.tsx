ce53724585c6828e18c63f86fe0790338e37c116
Cristian Cioanca <ccioanca@users.noreply.github.com>
2026-02-11 18:31:57 -0500

    Small Hero UI Update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
       className="h-screen flex items-center justify-center "
     >
       <div className="relative z-10 text-center px-4">
-        <h1 className="mb-4 leading-tight tracking-tight drop-shadow-md">
+        <h1 className="mb-4 tracking-wide drop-shadow-md">
           Hi, I'm <span className="text-primary dark:primary">Cristian</span>
         </h1>
         <div className="pb-12 text-2xl sm:text-3xl font-mono text-gray-700 dark:text-gray-300 drop-shadow-sm h-20 flex items-center justify-center"> {/* Added h-20 and flex for consistent height */}
@@ -28,7 +28,7 @@ const Hero = () => {
           <Button 
           size="xl" 
           variant="default"
-          className="mt-8 mx-auto" asChild>
+          className="mt-8 mx-auto md:mr-4" asChild>
           <a href='#contact'>
             Contact Me
           </a>
@@ -36,7 +36,7 @@ const Hero = () => {
         <Button 
           size="xl" 
           variant="outline"
-          className="ml-5 mt-8 mx-auto"
+          className="ml-5 mt-8 mx-auto md:ml-4"
           asChild>
           <a href="#projects">View My Work</a>
         </Button>
