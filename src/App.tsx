8d5f9cc83f0421b75375bc40fc25d51cc7b30e72
Cristian Cioanca <ccioanca@users.noreply.github.com>
2026-02-11 18:30:54 -0500

    Added theme provier for sonner

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,26 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import { Toaster } from '@/components/ui/sonner';
 import Projects from "./components/Projects";
+import { ThemeProvider } from "next-themes";
 
 
 function App() {
 
   return (
-    <div className="w-full min-h-screen relative">
-      <Header/>
-      <main>
-        <Hero />
-        <About />
-        <Projects />
-        <Contact />
-      </main>
-      <Footer/>
-      <Toaster />
-    </div>
+    <ThemeProvider>
+        <div className="w-full min-h-screen relative">
+        <Header/>
+        <main>
+            <Hero />
+            <About />
+            <Projects />
+            <Contact />
+        </main>
+        <Footer/>
+        <Toaster />
+        </div>
+    </ThemeProvider>
+    
   );
 }
 
