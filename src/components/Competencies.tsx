33c326df931d97b2b386e9a0615880b33a976135
Cris <cristian.cioanca@gmail.com>
2026-02-11 22:09:42 -0500

    Added more info to Competencies

--- src/components/Competencies.tsx
+++ src/components/Competencies.tsx
@@ -4,43 +4,76 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion"
+import { Badge } from "@/components/ui/badge";
+import { Progress } from "@/components/ui/progress";
 
 const items = [
   {
     value: "item-1",
     trigger: "Strategic Leadership",
+    tags: [
+      "Recruitment",
+      "Budgeting",
+      "Team Orchestration",
+      "Stakeholder Management",
+      "Agile Stewardship"
+    ],
     content: [
-      <span>test</span>
+      <ul className="list-inside">
+        <li className="list-disc mb-1">Successfully directing specialized teams and distributed remote staff across Canada to function as high-velocity units.</li>
+        <li className="list-disc mb-1">Championing Agile methodologies and implementing concrete delivery standards that have successfully raised sprint goal completion to over 90%.</li>
+        <li className="list-disc mb-1">Participate in Continuous Improvement and Change Management Boards to elevate communication standards and enable quick iteration across business units.</li>
+        <li className="list-disc mb-1">Acting as a bridge between technical teams and Product Owners to ensure predictable delivery, superior stakeholder alignment, and high satisfaction rates.</li>
+        <li className="list-disc mb-1">Managing budgetary allocation and resource distribution to ensure team efficiency and project feasibility.</li>
+      </ul>
     ],
   },
   {
     value: "item-2",
     trigger: "Technical Management",
+    tags: [
+      "CI/CD Management",
+      "Architecture Review", 
+      "Process Standardization",
+      "Technical Story Writing"
+    ],
     content:[
-      <span>test</span>
+      <ul className="list-inside">
+        <li className="list-disc mb-1">Guiding architectural decisions based on ecosystem needs rather than trends, providing high-level technical mentorship to complex teams.</li>
+        <li className="list-disc mb-1">Established delivery and release frameworks that increased deployment visibility, reduced post-deployment incidents, and accelerated time-to-market.</li>
+        <li className="list-disc mb-1">Leading rigorous architecture proposal reviews to ensure scalable system design, code extensibility, and the proactive reduction of technical debt.</li>
+        <li className="list-disc mb-1">Participate </li>
+        <li className="list-disc mb-1">Aligning application roadmaps with enterprise standards by coordinating with Security and Operations teams.</li>
+        <li className="list-disc mb-1">Identifying and implementing process improvements to drive iterative change and reduce friction in ways-of-working between product and development teams.</li>
+      </ul>
     ],
   },
   {
     value: "item-3",
-    trigger: "Backend Development",
-    content: [
-      <span>test</span>
+    trigger: "Technical & Development Skills",
+    tags: [
+      ".NET Core",
+      "C#",
+      "SQL",
+      "Restful APIs",
+      "React",
+      "Typescript",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "UI/UX Strategy",
+      "CI/CD Pipelines",
+      "Microservices"
     ],
-  },
-  {
-    value: "item-4",
-    trigger: "Frontend Development",
     content: [
-      <span>test</span>
+      <ul className="list-inside">
+        <li className="list-disc mb-1">Expert proficiency in .NET Core, C#, and SQL, with a focus on building scalable microservices and high-performance RESTful APIs.</li>
+        <li className="list-disc mb-1">Deep experience owning the lifecycle of services from requirement definition to national deployment on Microsoft Azure.</li>
+        <li className="list-disc mb-1">Expert proficiency in front-end technologies and principles such as SEO, accessibility, and responsive design.</li>
+        <li className="list-disc mb-1">Technical management of the Adobe stack, including Adobe Experience Manager (AEM), Customer Data Platform (CDP), Adobe Campaign Standard APIs (ACS), Analytics, and Target.</li>
+      </ul>
     ],
-  },
-  {
-    value: "item-5",
-    trigger: "Design",
-    content: [
-      <span>test</span>
-    ],
-  },
+  }
 ]
 
 
@@ -49,8 +82,23 @@ const Competencies = () => {
       <Accordion type="single" collapsible={true} className="rounded-lg border bg-card">
         {items.map((item) => (
           <AccordionItem key={item.value} value={item.value} className="border-b px-4 last:border-b-0">
-            <AccordionTrigger className="cursor-pointer text-lg">{item.trigger}</AccordionTrigger>
-            <AccordionContent>{item.content}</AccordionContent>
+            <AccordionTrigger className="cursor-pointer text-lg flex-col md:flex-row">
+              <div className="mr-3">
+                {item.trigger}
+              </div>
+              
+              </AccordionTrigger>
+            <AccordionContent>
+              <div className="mb-5">
+                {item.tags?.map((tag) => (
+                  <Badge key={tag} className="bg-chart-2 mr-2 mb-2">
+                    {tag}
+                  </Badge>
+                ))}
+              </div>
+              {(item.tags && item.content) && <hr className="mb-6" />}
+              {item.content}
+            </AccordionContent>
           </AccordionItem>
         ))}
       </Accordion>
